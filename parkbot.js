10a1d2e95838f2cbe20459c5f665a27960323424
mattdean <matthewallandean@gmail.com>
2021-04-22 14:26:49 -0600

    Clean up function

--- parkbot.js
+++ parkbot.js
@@ -1,9 +1,8 @@
 #!/usr/bin/env node parkbot
-const rawData = require("./data");
+const rawData = require("./airgarage-data");
 const data = JSON.parse(rawData);
 
 let args = process.argv.filter((val, index) => index >= 3);
-
 
 function mapNames(mapData) {
   return mapData.map(location => location.name)
@@ -11,12 +10,12 @@ function mapNames(mapData) {
 
 function returnData() {
   let result;
-  // args[0] checks for what data file to access, avoids badly formatted commands
-  if (args[0] === 'data.json') {
+
+  if (args[0] === 'airgarage-data.json') {
     if (args[1] === 'locate') {
       result = data.filter(location => location.address.state === args[2]);
     }
-    
+
     if (args[1] === 'find_price_hourly_lte') {
       result = data.filter(location => location.price_hourly < args[2])
     }
@@ -25,7 +24,7 @@ function returnData() {
       result = data.filter(location => location.price_hourly > args[2])
     }
   }
-
+  
   console.log(mapNames(result))
 } 
 
